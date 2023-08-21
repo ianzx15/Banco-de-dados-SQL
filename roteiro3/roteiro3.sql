5b6a8d6a71a78bff1f5220dbdaf849ea4952d8b2
ian <ian.rodrigues>
2023-08-20 22:52:50 -0300

    u

--- roteiro3/roteiro3.sql
+++ roteiro3/roteiro3.sql
@@ -1,39 +1,60 @@
-CREATE TABLE tarefas (
-    id INTEGER,
-    descriacao VARCHAR(50), 
-    func_resp_cpf VARCHAR(11),
-    prioridade INTEGER, 
-    status CHAR(1)
+
+CREATE TYPE ESTADOS AS ENUM('PB','PE','MA', 'PI', 'CE', 'RN', 'AL', 'SE', 'BH');
+CREATE TYPE PROFISSAO AS ENUM('FA', 'VE', 'EN', 'CA', 'AD');
+
+CREATE TABLE farmacias(
+        id_farmacia INTEGER PRIMARY KEY,
+        tipo_farmacia CHAR(1) CHECK(tipo_farmacia IN ('F', 'S')), EXCLUDE USING gist(
+        tipo_farmacia with=) WHERE (tipo_farmacia = 'S'),
+        bairro VARCHAR(50) UNIQUE,
+        cidade VARCHAR(50),
+        estado ESTADOS,
+        tem_gerente BOOLEAN CHECK (tem_gerente = TRUE, ),
+        id_funcionario INTEGER REFERENCES funcionarios (isGerente),
+        EXCLUDE USING gist(
+            isGerente with=) WHERE (isGerente = TRUE)
+        );
+    
+CREATE TABLE funcionarios(
+    id_farmacia INTEGER REFERENCES farmacias (id_farmacia),
+    id_funcionario INTEGER,
+    profissao PROFISSAO,
+    isGerente TEXT CHECK(isGerente = 'TRUE' AND profissao in ('FA', 'AD')),
+    PRIMARY KEY (id_funcionario, isGerente)
 );
 
-INSERT INTO tarefas VALUES(null, null, null, null, null);
+CREATE TABLE clientes(
+    cpf_cliente VARCHAR(11) CHECK(char_length (cpf_cliente) = 11) PRIMARY KEY,
+    nascimento DATE,
+    idade TEXT IS current_date - nascimento,
+    conta_valida BOOLEAN,
+    CHECK (idade >= 18 AND conta_valida = TRUE)
+
+);
+
+CREATE TABLE enderecos(
+    cep_endereco INTEGER (8) CHECK(char_length (cep_endereco) = 8),
+    cpf_cliente VARCHAR(11) REFERENCES clientes (cpf_cliente),
+    tipo_endereco TEXT CHECK(tipo_endereco IN ('residência', 'trabalho', 'outro'))
+);
 
 
-ALTER TABLE tarefas ALTER COLUMN id TYPE BIGINT;
 
-ALTER TABLE tarefas  ADD CONSTRAINT prioridade_chk CHECK (prioridade < 32768);
+CREATE TABLE medicamentos(
+    id_medicamento INTEGER,
+    com_receita BOOLEAN
 
-DELETE FROM tarefas WHERE id IS NULL;
 
-ALTER TABLE tarefas ALTER COLUMN prioridade SET NOT NULL;
+);
 
-DELETE FROM tarefas  WHERE prioridade = 3;
+CREATE TABLE entregas(
+    id_entrega INTEGER(50),
+    cpf_cliente VARCHAR(11) REFERENCES clientes (cpf_cliente),
+    cep_endereco VARCHAR(8) REFERENCES enderecos (cep_endereco)
 
-ALTER TABLE tarefas ADD PRIMARY KEY (id);
+);
 
-ALTER TABLE tarefas ADD CONSTRAINT func_resp_cpf_chk CHECK (char_length(func_resp_cpf) = 11)
-
-UPDATE tarefas SET status = 'P' WHERE status = 'A';
-
-ALTER TABLE tarefas  ADD CONSTRAINT status_chk CHECK(status in ('P', 'E', 'C'));
-
-ALTER TABLE tarefas ADD CONSTRAINT prioridade_chk2 CHECK(prioridade in (0,1,2,3,4,5));
-
-CREATE TABLE funcionario(
-    cpf VARCHAR(11) CHECK (char_length = 11) PRIMARY KEY,
-    data_nasc DATE NOT NULL,
-    nome VARCHAR(30) NOT NULL,
-    funcao VARCHAR,
-    nivel CHECK (nivel in ('S', 'P', 'S')),
-    superior_cpf  REFERENCES funcionario cpf,
-)
+CREATE TABLE vendas(
+    id_medicamento INTEGER REFERENCES medicamentos (id_medicamento),
+    cpf_cliente VARCHAR(11) REFERENCES clientes (cpf_cliente)
+);
